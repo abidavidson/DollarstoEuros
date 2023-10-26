edae44f5e409aaa7f8226f4c99248dea6d86dd83
abidavidson <ad082705@wgcloud.org>
2023-10-26 09:34:39 -0500

    added conversion code

---
+++ dollarstoeuros.py
@@ -0,0 +1,9 @@
+print("Program: Dollar to Euro Converter (v1)")
+print("This program will ask users for a dollar amount and then convert it to euros. They can continue to continue as long as they want to.") 
+
+user_conversion=input("Would you like to convert dollars to euros? ")
+while user_conversion == "yes":
+    dollar=float(input("Enter dollar amount to be converted: "))
+    euro = dollar * .94540
+    print("€" + str(euro))
+    user_conversion=input("Would you like to convert dollars to euros? ")
